64f18279093bbdc4e01ae99abb41f21d70e19148
pdjjzkxtgk-max <pdjjzkxtgk@privaterelay.appleid.com>
2026-04-15 11:00:37 +0200

    design: add spacing constants on 4 pt grid

---
+++ lib/theme/app_spacing.dart
@@ -0,0 +1,18 @@
+abstract final class AppSpacing {
+  static const double xs = 4.0;
+  static const double sm = 8.0;
+  static const double md = 16.0;
+  static const double lg = 24.0;
+  static const double xl = 32.0;
+  static const double xxl = 48.0;
+
+  // Corner radii
+  static const double radiusSm = 10.0;
+  static const double radiusMd = 14.0;
+  static const double radiusLg = 20.0;
+  static const double radiusXl = 28.0;
+  static const double radiusFull = 100.0;
+
+  // Screen horizontal padding
+  static const double screenPadding = 20.0;
+}
